e6d7d8b7ff48b76c3f458b9628debf42c073c745
GS-0955 <GS-0955@GS-2771.GSLAB.COM>
2017-03-06 16:37:01 +0530

    Main class

---
+++ TimeSheetAutomation/src_Main_Java_Resources/timesheetApp_landingPage/MainClass.java
@@ -0,0 +1,24 @@
+package timesheetApp_landingPage;
+
+class MainClass
+{
+	
+	public void LoginTestCases() throws Exception
+	{
+	TestLandingPage testLanding = new TestLandingPage();
+	testLanding.testLandingPageElementsPresent();
+	testLanding.testLandingPageTitle();
+	testLanding.testValidLogin();
+	testLanding.testLoginPageElementsPresent();
+	testLanding.testUserGetsTaksDetailsPopupOnNewTaskAddition();
+	testLanding.testValidEntryadded();
+	testLanding.testAllEntriesDisplayed();
+	testLanding.test5EntriesDisplayed();
+	testLanding.test7EntriesDisplayed();
+	testLanding.testValidLogout();
+	
+	}
+
+
+
+}
